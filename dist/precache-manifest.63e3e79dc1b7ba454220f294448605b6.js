319c85071868a281faeecb392b3b4b79616c1181
Uddeshya Agrawal <uddeshyacool50@gmail.com>
2020-08-11 00:53:14 +0530

    first push

---
+++ dist/precache-manifest.63e3e79dc1b7ba454220f294448605b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d251d2f56bac9dbd8161",
+    "url": "css/app.276c46a2.css"
+  },
+  {
+    "revision": "d251d2f56bac9dbd8161",
+    "url": "js/app.eb4af8ff.js"
+  },
+  {
+    "revision": "c00aa80d34b334474459",
+    "url": "js/chunk-vendors.2d1eee83.js"
+  },
+  {
+    "revision": "f89201b98a9a3ac73289973032659a60",
+    "url": "index.html"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  }
+];
---
+++ src/config.js
@@ -0,0 +1,56 @@
+import { get_gateway_url } from './utils'
+
+export default {
+    app: {
+        gateway: get_gateway_url('https://taltowidget-mshu.core.ushaflow.io'), // <- enter your gateway URL here, the function is just a helper function for my hosted integration. You don't normally need it
+        muted: false, // <- mute microphone at start
+        start_suggestions: ["Hi"], // <- array of suggestions, displayed at the start screen
+        fallback_lang: 'en', // <- fallback language code, if history mode or network is unavailable,
+        voice: 'native' // <- voice of Text-To-Speech; for reference, see: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice/voiceURI
+    },
+    i18n: {
+        en: {
+            welcomeTitle: 'Welcome to',
+            muteTitle: 'Mute',
+            unMuteTitle: 'Unmute',
+            inputTitle: 'Type your message',
+            sendTitle: 'Send',
+            microphoneTitle: 'Voice Input',
+            recognitionUnavailable: 'Speech recognition is not available'
+        },
+        ru: {
+            welcomeTitle: 'Добро пожаловать в',
+            muteTitle: 'Звук',
+            unMuteTitle: 'Без звука',
+            inputTitle: 'Введите ваше сообщение',
+            sendTitle: 'Отправить',
+            microphoneTitle: 'Голосовой ввод'
+        },
+        de: {
+            welcomeTitle: 'Wilkommen bei',
+            muteTitle: 'Stumm',
+            unMuteTitle: 'Nicht stumm',
+            inputTitle: 'Schreiben Sie ihre Nachricht',
+            sendTitle: 'Senden',
+            microphoneTitle: 'Spracheingabe'
+        },
+        fr: {
+            welcomeTitle: 'Bienvenue à',
+            muteTitle: 'Son',
+            unMuteTitle: 'Silent',
+            inputTitle: 'Entrez votre message',
+            sendTitle: 'Envoyer',
+            microphoneTitle: 'Entrée vocale'
+        },
+        hi: {
+
+            welcomeTitle: 'स्वागत है',
+            muteTitle: 'Mute',
+            unMuteTitle: 'Unmute',
+            inputTitle: 'अपना सन्देश प्रकार करे',
+            sendTitle: 'send',
+            microphoneTitle: 'Voice Input'
+
+        }
+    }
+}
